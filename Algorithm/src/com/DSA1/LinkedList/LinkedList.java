b149b368e3280f64d6f93f8f5c3e2fa8e62843d7
joe19990131 <32476180+joe19990131@users.noreply.github.com>
2018-10-15 23:07:34 +0800

    LinkedList_Insert Part

--- Algorithm/src/com/DSA1/LinkedList/LinkedList.java
+++ Algorithm/src/com/DSA1/LinkedList/LinkedList.java
@@ -20,19 +20,9 @@ class Node {
 public class LinkedList {
 	private Node first;
 	private Node last;
-
+	Node ptr;
 	public boolean isEmpty() {
 		return first == null;
-	}
-
-	public void print() {
-		Node current = first;
-		while (current != null) {
-			System.out.println("[" + current.no + " " + current.name + " " + current.score + "]");
-
-			current = current.next;
-		}
-		System.out.println("");
 	}
 
 	public void insert(int no, String name, int score) {
@@ -45,13 +35,46 @@ public class LinkedList {
 			last = newNode;
 		}
 	}
-	
+
+	public void headInsert(int no, String name, int score) {
+		Node newNode = new Node(no, name, score);
+		newNode.next = first;
+		first = newNode;
+	}
+
+	public void endInsert(int no, String name, int score) {
+		Node newNode = new Node(no, name, score);
+		last.next = newNode;
+		last = newNode;
+	}
+
+	public void midInsert(int no, String name, int score,Node ptr) {
+		Node newNode = new Node(no, name, score);
+		newNode = first;
+		Node tmp = first;
+		while(ptr.next!=newNode.next) {
+			tmp = newNode;
+			newNode = newNode.next;
+		}
+		tmp.next = ptr;
+		ptr.next = newNode;
+	}
+
+	public void print() {
+		Node current = first;
+		while (current != null) {
+			System.out.println("[" + current.no + " " + current.name + " " + current.score + "]");
+
+			current = current.next;
+		}
+		System.out.println("");
+	}
+
 	public static void main(String args[]) {
 		BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
 		int no;
 		String name;
 		int score;
-		
-		
+
 	}
 }
